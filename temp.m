c5a8d4e26c258733bf693710f97ecbd641ffc97b
Chutli <kimhoang_hn@yahoo.com>
2014-10-11 22:51:04 +0700

    Dung ifft de khoi phuc

--- temp.m
+++ temp.m
@@ -2,5 +2,19 @@ clear all
 clc
 load data1trieu.mat data;
 plot(data)
-spec = fft(data);
-plot(spec)
+ftdata = fft(data)/length(data);
+amftdata = 2*abs(ftdata);
+for i = 1: length(ftdata)
+    if amftdata(i) <= 10
+        ftdata(i) = 0;
+    end
+end
+ivdata = ifft(ftdata*length(data));  
+hold on
+plot(ivdata,'r')
+for i = 1:length(data)
+    Bias(i) = (data(i) - ivdata(i))/data(i)*100;
+end
+mean Bias
+
+    
